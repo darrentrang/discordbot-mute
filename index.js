b28e28490a27210aef016c149be6b459a4714110
Darren Trang <darrentrang93@gmail.com>
2021-05-19 17:08:16 -0700

    add config.json to .gitignore. modify index.js to use discord app login token from heroku config var instead of using config.json file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '!';
 const fs = require('fs');
-const config = require('./config.json')
+//const config = require('./config.json')  //hide from public github repo
 
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
@@ -37,5 +37,5 @@ client.on('message', message=>{
 	
 });
 
-client.login(config.token);
+client.login(process.env.TOKEN);  //use heroku config var instead of external config.json file
 
